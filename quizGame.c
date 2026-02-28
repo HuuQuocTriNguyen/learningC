25724e26556ca9fe8c00896a5431fc1aeb545089
Christopher nguyen <chrisnguyen1304@Christophers-MacBook-Pro.local>
2026-02-28 14:38:00 -0700

    new project

---
+++ quizGame.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    char questions[][100] = {
+        "What is the largest planet in the solar system?",
+        "What is the hottest planet?",
+        "What planets has the most moon?",
+        "Who is the most goated JJK Character? "
+    }; 
+
+    char options[][100] = {
+        "A. Jupiter\nB. Saturn\nC. Uranus\nD. Neptune",
+        "A. Mercury\nB. Venus\nC. Earth\nD. Mars",
+        "A. Earth\nB. Mars\nC. Jupiter\nD. Saturn",
+        "A. Gojo\nB. Sukuna\nC. Yuji\nD. Mahogara"
+    };
+
+    char answerKeys[] = {'A', 'B', 'D', 'A'};
+    char userChoice = '\0';
+    int points = 0;
+    int questionCounts = sizeof(questions) / sizeof(questions[0]);
+
+    printf("*** QUIZ GAME ***\n");
+    for (int i = 0; i < questionCounts; i++) {
+        printf( 
+            "\n%s\n"
+            "%s\n"
+            "Enter your Choice: "
+        , questions[i], options[i]);
+        scanf(" %c", &userChoice);
+        
+        if (userChoice == answerKeys[i]) {
+            printf("\nCORRECT!\n");
+            points++;
+        } else {
+            printf("\nWRONG!\n");
+        }
+    }
+
+    printf("\nYour score is %i out of %i points.\n", points, questionCounts);
+
+    return 0;
+}
